23aa5500990afab585a062f8c1122de2d0fa5072
Evan Herman <Evan@Evans-MacBook-Pro.local>
2016-12-24 20:28:47 -0500

    Remove all transients from the database on plugin uninstall.

--- uninstall.php
+++ uninstall.php
@@ -17,7 +17,10 @@ global $wpdb;
 
 $wpdb->query(
 	$wpdb->prepare(
-		'DELETE FROM ' . $wpdb->options . ' WHERE option_name LIKE "%s"',
-		$wpdb->esc_like( '_transient_invalid_login_' ) . '%'
+		'DELETE FROM ' . $wpdb->options . '
+		WHERE option_name LIKE "%1$s"
+		OR option_name LIKE "%2$s"',
+		$wpdb->esc_like( '_transient_invalid_login_' ) . '%',
+		$wpdb->esc_like( '_transient_timeout_invalid_login_' ) . '%'
 	)
 );
