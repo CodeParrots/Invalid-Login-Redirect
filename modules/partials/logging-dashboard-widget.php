48e72a72d106941a0739cded3d0886cbe10fb988
Ben Rothman <brothman01@gmail.com>
2017-01-02 18:51:06 -0500

    this is so that the upstream merge will work

--- modules/partials/logging-dashboard-widget.php
+++ modules/partials/logging-dashboard-widget.php
@@ -5,14 +5,41 @@
  * @since 1.0.0
  */
 
+wp_enqueue_style( 'ilr-login-style', ILR_URL . '/lib/css/ilr-admin.css' );
+
 if ( ! defined( 'ABSPATH' ) ) {
 
 	exit;
 
 }
 
-$logs = parent::$helpers->get_ilr_log( 2 );
+$logs = parent::$helpers->get_ilr_log( 10 );
 
-?>
+if ( $logs->have_posts() ) {
 
-<strong>Ben, the markup will go here.</strong>
+	echo '<table class="ilr-widget-table" style="width: 100%;">
+	  <tr>
+	    <th class="tg-yw4l">Date/Time</th>
+	    <th class="tg-yw4l">Username</th>
+	    <th class="tg-yw4l">Attempt</th>
+	    <th class="tg-yw4l">Type<br></th>
+	  </tr>';
+
+	while ( $logs->have_posts() ) {
+
+		$logs->the_post();
+
+		echo '<tr class="ilr-widget-row">
+				    <td class="ilr-widget-cell">' . date( get_option( 'date_format' ), get_post_meta( get_the_ID(), 'ilr_log_timestamp', true ) ) . ' &ndash; ' . date( get_option( 'time_format' ), get_post_meta( get_the_ID(), 'ilr_log_timestamp', true ) ) . '</td>
+				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_username', true ) . '</td>
+				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_attempt', true ) . '</td>
+				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_type', true ) . '</td>
+		    </tr>';
+
+	}
+
+	echo '</table>';
+
+	wp_reset_postdata();
+
+}
