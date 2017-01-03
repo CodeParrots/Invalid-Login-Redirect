eceb52810587063df3dfbd97bfad66b120cd360d
Ben Rothman <brothman01@gmail.com>
2017-01-02 23:13:09 -0500

    resolved conflicts

--- modules/partials/logging-dashboard-widget.php
+++ modules/partials/logging-dashboard-widget.php
@@ -17,21 +17,23 @@ $logs = parent::$helpers->get_ilr_log( 10 );
 
 if ( $logs->have_posts() ) {
 
-	echo '<table class="ilr-widget-table" style="width: 100%;">
-	  <tr>
-	    <th class="tg-yw4l">Date/Time</th>
-	    <th class="tg-yw4l">Username</th>
-	    <th class="tg-yw4l">Attempt</th>
-	    <th class="tg-yw4l">Type<br></th>
-	  </tr>';
+	echo '<table class="ilr-widget-table wp-list-table widefat fixed striped logs" style="width: 100%;">
+		<thead>
+		  <tr>
+		    <th class="tg-yw4l">Date/Time</th>
+		    <th class="tg-yw4l">Username</th>
+		    <th class="tg-yw4l">Attempt</th>
+		    <th class="tg-yw4l">Type<br></th>
+		  </tr>
+		</thead>';
 
 	while ( $logs->have_posts() ) {
 
 		$logs->the_post();
 
 		echo '<tr class="ilr-widget-row">
+						<td class="ilr-widget-cell" style="text-align: left!important;">' . get_post_meta( get_the_ID(), 'ilr_log_username', true ) . '</td>
 				    <td class="ilr-widget-cell">' . date( get_option( 'date_format' ), get_post_meta( get_the_ID(), 'ilr_log_timestamp', true ) ) . ' &ndash; ' . date( get_option( 'time_format' ), get_post_meta( get_the_ID(), 'ilr_log_timestamp', true ) ) . '</td>
-				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_username', true ) . '</td>
 				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_attempt', true ) . '</td>
 				    <td class="ilr-widget-cell">' . get_post_meta( get_the_ID(), 'ilr_log_type', true ) . '</td>
 		    </tr>';
