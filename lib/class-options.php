260d85f9d59085cfa6dd47d95d810c3318c212e1
Ben Rothman <brothman01@gmail.com>
2017-01-05 16:57:39 -0500

    changed wording in email notification module

--- lib/class-options.php
+++ lib/class-options.php
@@ -575,7 +575,7 @@ class Invalid_Login_Redirect_Settings {
 				'file'        => 'class-user-role-redirects.php',
 				'description' => __( 'Redirect specific user roles to certain areas of your site after a successful login.', 'invalid-login-redirect' ),
 			],
-			__( 'Notifications', 'invalid-login-redirect' ) => [
+			__( 'Email Notifications', 'invalid-login-redirect' ) => [
 				'file'        => 'class-notifications.php',
 				'description' => __( 'Display a notice to the admin users whenever a logged action occurs.', 'invalid-login-redirect' ),
 			],
